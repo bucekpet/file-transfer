4f83bba90271b965045244a96cce3c5fa01842db
Jebingo <58402893+Jebingo@users.noreply.github.com>
2023-11-14 16:30:37 +0100

    init

---
+++ src/components/QrCode.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <img class="qr-code" v-if="qrCodeDataUrl" :src="qrCodeDataUrl" alt="QR Code">
+    </div>
+</template>
+
+<script>
+import QRCode from 'qrcode-generator';
+
+export default {
+    data() {
+        return {
+            qrCodeDataUrl: null,
+        };
+    },
+    mounted() {
+        this.generateQRcode()
+    },
+    methods: {
+        generateQRcode() {
+            const text = "http://192.168.0.161:8888"
+            const cell_size = 5
+
+            const qr = QRCode(0, 'M')
+            qr.addData(text)
+            qr.make()
+
+            const dataUrl = qr.createDataURL(cell_size, 0)
+            this.qrCodeDataUrl = dataUrl
+        }
+    }
+}
+</script>
+
+<style scoped>
+.qr-code {
+    filter: invert(100%);
+    /* Invert the colors to make black green */
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import QrCode from '../components/QrCode.vue';
+</script>
+
+<template>
+  <div class="about">
+    <QrCode />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
