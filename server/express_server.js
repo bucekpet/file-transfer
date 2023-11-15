3d4587e865a7df31535d8bc13ceaf549e7602539
Jebingo <58402893+Jebingo@users.noreply.github.com>
2023-11-15 17:16:02 +0100

    added dependencies

--- server/express_server.js
+++ server/express_server.js
@@ -6,12 +6,12 @@ const ip = require('ip')
 const app = express()
 const port = 3000
 
-const folderPath = './input_files'
+const downloadFolderPath = './input_files'
 
 app.use(cors())
 
 app.get('/api/files', (req, res) => {
-  fs.readdir(folderPath, (err, files) => {
+  fs.readdir(downloadFolderPath, (err, files) => {
     if (err) {
       console.error(err)
       res.status(500).send('Internal Server Error')
@@ -23,7 +23,7 @@ app.get('/api/files', (req, res) => {
 
 app.get('/api/download/:filename', (req, res) => {
   const { filename } = req.params
-  const filePath = `${folderPath}/${filename}`
+  const filePath = `${downloadFolderPath}/${filename}`
 
   // Set appropriate headers for downloading
   res.setHeader('Content-Disposition', `attachment filename=${filename}`)
